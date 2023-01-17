3a34485e9a572e259cc426f0bd96542a7bc40837
numan-kode <122619931+numan-kode@users.noreply.github.com>
2023-01-18 00:08:14 +0530

    first

---
+++ index.php
@@ -0,0 +1,167 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Paan Shop</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <?php include "components/header.php" ?>
+  <div class="container">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="images/a1.jpg" height="500" width="1550" class="d-block  " alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            
+            <h3>
+              <p1>Paan Maker</p1>
+            </h3>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="images/a2.jpg" height="500" width="1550" class="d-block  " alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            
+            <h3>
+              <p1>Paan Distributer and Supplies</p1>
+            </h3>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="images/a3.jpeg" width="1550" height="500"  alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            
+            <h3>
+              <p1>Paan Events</p1>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+
+  <h1 class="text-center"> Our Services </h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 p-2">
+      <div class="card border-0 shadow rounded-5">
+        <div class="image rounded-4"></div>
+        <div class="card-body">
+          <h5 class="card-title">Paan Catering Services For Wedding</h5>
+          
+        </div>
+      </div>
+      </div>
+      
+      
+      <div class="col-md-4 p-2">
+      <div class="card border-0 shadow rounded-5">
+        <div class="image5 rounded-5"></div>
+        
+        <div class="card-body">
+          <h5 class="card-title">Paan Catering Services For Reception Party</h5>
+          
+        </div>
+      </div>
+      </div>
+      
+        
+      <div class="col-md-4 p-2">
+      <div class="card border-0 shadow rounded-5">
+        <div class="image6 rounded-5"></div>
+        <div class="card-body">
+          <h5 class="card-title">Paan Catering Services For Kitty Party</h5>
+          
+        </div>
+      </div>
+      </div>
+      
+        
+      <div class="col-md-4 p-2">
+      <div class="card border-0 shadow rounded-5">
+        <div class="image7 rounded-5"></div>
+        <div class="card-body">
+          <h5 class="card-title">Paan Catering Services For Events Party</h5>
+          
+        </div>
+      </div>
+      </div>
+      
+        
+      <div class="col-md-4 p-2">
+      <div class="card border-0 shadow rounded-5">
+        <div class="image8 rounded-5"></div>
+        <div class="card-body">
+          <h5 class="card-title">Paan Catering Services For Birthday Party</h5>
+          
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-intervel="10">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/logo.jpg"  >
+    </div>
+    <div class="carousel-item">
+      <img src="images/a1.jpg" >
+    </div>
+    <div class="carousel-item">
+      <img src="images/a2.jpg" >
+    </div>
+  </div> -->
+</div>
+
+<div class="container mt-5">
+  <h2 class="text-center">Most Popular Course</h2>
+  <div class="row">
+    <div class="col">
+     
+    <div class="row">
+        <div class="col">
+    <div class="box bg-dark d-flex justify-content-center align-items-center"  style="width: 200px; height: 200px;" ><p2 class="text-center">Diploma In Project Management</p2></div>
+        </div>
+        <div class="col">
+        <div class="box bg-primary d-flex justify-content-center align-items-center" style="width: 200px; height: 200px;"><p2 class="text-center">Mental Health Studies</p2></div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+        <div class="box bg-secondary d-flex justify-content-center align-items-center" style="width: 200px; height: 200px;"> <p2 class="text-center">Introduction to Project Management</p2></div>
+        </div>
+        <div class="col">
+        <div class="box bg-danger d-flex justify-content-center align-items-center" style="width: 200px; height: 200px;"> <p2 class="text-center">Online Diplomain Psychology</p2></div>
+        </div>
+      </div>
+    
+    
+    
+    </div>
+  </div>
+</div>
+
+  <?php include "components/footer.php" ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
